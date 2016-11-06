73c71d1561787da2efe98298d7ad9f7fe58fe857
Clement CHAMPOUILLON <cchampou@e1r3p8.42.fr>
2016-11-06 07:13:30 +0100

    ft_memmove

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: cchampou <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/11/04 20:26:57 by cchampou          #+#    #+#              #
-#    Updated: 2016/11/06 04:20:51 by cchampou         ###   ########.fr        #
+#    Updated: 2016/11/06 07:11:57 by cchampou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -29,7 +29,8 @@ C_FILES = 	ft_strlen.c		\
 			ft_memset.c		\
 			ft_bzero.c		\
 			ft_memcpy.c		\
-			ft_memccpy.c
+			ft_memccpy.c	\
+			ft_memmove.c
 
 O_FILES = $(C_FILES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cchampou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 19:20:10 by cchampou          #+#    #+#             */
-/*   Updated: 2016/11/06 04:22:02 by cchampou         ###   ########.fr       */
+/*   Updated: 2016/11/06 07:12:16 by cchampou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memccpy(void *restrict dst, const void *restrict src,
 		int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
