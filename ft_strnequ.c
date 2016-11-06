aea48124ee5f713da141b4d7870c56f8dea2ab81
Clement CHAMPOUILLON <cchampou@e1r3p8.42.fr>
2016-11-06 14:39:38 +0100

    ft_strnequ

---
+++ ft_strnequ.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchampou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/06 14:37:52 by cchampou          #+#    #+#             */
+/*   Updated: 2016/11/06 14:39:13 by cchampou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+	size_t	length;
+	size_t	i;
+
+	i = 0;
+	length = (size_t)ft_strlen(s1);
+	if (length != (size_t)ft_strlen(s2))
+		return (0);
+	while (i < length && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cchampou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 19:20:10 by cchampou          #+#    #+#             */
-/*   Updated: 2016/11/06 14:30:59 by cchampou         ###   ########.fr       */
+/*   Updated: 2016/11/06 14:37:46 by cchampou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,5 +56,6 @@ void	ft_striteri(char *s, void (*f) (unsigned int, char*));
 char	*ft_strmap(char const *s, char (*f) (char));
 char	*ft_strmapi(char const *s, char (*f) (unsigned int, char));
 int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
 
 #endif
