7d2d4b4de33f1e5044ba8ad8644b14871b1534ba
Clement CHAMPOUILLON <cchampou@e1r3p8.42.fr>
2016-11-24 19:19:40 +0100

    Correction strncpy, strnequ

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: cchampou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 14:37:52 by cchampou          #+#    #+#             */
-/*   Updated: 2016/11/24 19:12:49 by cchampou         ###   ########.fr       */
+/*   Updated: 2016/11/24 19:19:08 by cchampou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!s1 || !s2)
 		return (0);
-	if (!ft_strncmp(s1 ,s2 ,n))
+	if (!ft_strncmp(s1, s2, n))
 		return (1);
 	else
 		return (0);
