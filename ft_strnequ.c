cabffacc2d7f543fd2722f4e82ffe31a413e0c87
Clement CHAMPOUILLON <cchampou@e1r3p8.42.fr>
2016-11-24 19:13:09 +0100

    Correction strnequ

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: cchampou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 14:37:52 by cchampou          #+#    #+#             */
-/*   Updated: 2016/11/23 19:06:50 by cchampou         ###   ########.fr       */
+/*   Updated: 2016/11/24 19:12:49 by cchampou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,10 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	size_t	length;
-	size_t	i;
-
 	if (!s1 || !s2)
 		return (0);
-	i = 0;
-	if (n == 0)
+	if (!ft_strncmp(s1 ,s2 ,n))
 		return (1);
-	length = (size_t)ft_strlen(s1);
-	if (length != (size_t)ft_strlen(s2))
+	else
 		return (0);
-	while (i < length && i < n)
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (1);
 }
