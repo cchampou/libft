33769c3060c28577dc553f3aa7c86f16820cbaba
Clement CHAMPOUILLON <cchampou@e1r3p8.42.fr>
2016-11-24 19:07:58 +0100

    strncopy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,33 +6,25 @@
 /*   By: cchampou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 02:25:15 by cchampou          #+#    #+#             */
-/*   Updated: 2016/11/06 02:40:51 by cchampou         ###   ########.fr       */
+/*   Updated: 2016/11/24 19:07:34 by cchampou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "libft.h"
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	size_t	srclen;
-	size_t	dstlen;
 	size_t	i;
 
 	i = 0;
-	srclen = ft_strlen(src) + 1;
-	dstlen = ft_strlen(dst) + 1;
-	while (i <= dstlen && i < srclen && i < len)
+	while (src[i] && i < len)
 	{
-		if (i < srclen)
-		{
-			dst[i] = src[i];
-		}
+		dst[i] = src[i];
 		i++;
 	}
 	while (i < len)
 	{
-		dst[i] = '\0';
+		dst[i] = 0;
 		i++;
 	}
 	return (dst);
