8d6a3d951e6767c3fa9f654346a6f10f1ab46222
Clement CHAMPOUILLON <cchampou@e1r4p10.42.fr>
2016-11-26 15:36:07 +0100

    strnequ

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: cchampou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 14:37:52 by cchampou          #+#    #+#             */
-/*   Updated: 2016/11/26 15:22:48 by cchampou         ###   ########.fr       */
+/*   Updated: 2016/11/26 15:34:48 by cchampou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!s1 || !s2)
 		return (0);
-	return (ft_strncmp(s1, s2, n));
+	if (!ft_strncmp(s1, s2, n))
+		return (1);
+	else
+		return (0);
 }
